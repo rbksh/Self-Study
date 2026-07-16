03980585cab040c5c81bf57713af5b3520d3ee9d
Shesh Shiromani <rbkshesh@gmail.com>
2026-07-16 21:37:17 +0530

    .

---
+++ MIT 6.001 Introduction to Computer Science and Programming in Python/Lecture 1.py
@@ -0,0 +1,16 @@
+#trying something new
+
+import random
+a = random.randint(3,9)
+b = random.randint(3,9)
+n=int(input("please enter the number of repetitions: "))
+if type(n) != int:
+    print("invalid output")
+print("value of a: ", a)
+print("value of b: ", b)
+
+c = (a+b)/2
+
+for i in range(1,n):
+    if type(c)
+    i+=1
